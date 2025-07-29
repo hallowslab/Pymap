f20268e74487992e8ac55a1cd8d486aa2ea1b1bc
hallowslab <hallows.tech.lab@gmail.com>
2025-07-29 21:03:01 +0100

    fix way of getting user without breaking type checking

--- src/pymap/management/commands/reset_pass.py
+++ src/pymap/management/commands/reset_pass.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandParser
-from django.contrib.auth.models import User, AbstractBaseUser
+from django.contrib.auth import get_user_model
 from django.utils.crypto import get_random_string
 
 
@@ -23,8 +23,9 @@ class Command(BaseCommand):
         username = options["username"]
         length = options["length"]
         try:
-            user = User.objects.get(username=username)
-            assert isinstance(user, AbstractBaseUser)
+            UserModel = get_user_model()
+            user = UserModel.objects.get(username=username)
+
             _new_password = get_random_string(length)
             user.set_password(_new_password)
             if not user.check_password(_new_password):
