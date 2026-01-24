83c9c694b3f49415a112f435792f056f9bbdab0b
hallowslab <hallow.tech.lab@gmail.com>
2026-01-24 22:45:38 +0000

    update AppConfig

--- pymap/apps.py
+++ pymap/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PymapConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pymap"
+    verbose_name = "Pymap"
+    is_modular = True
