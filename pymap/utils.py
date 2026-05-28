d12fd528406b3ac5e1addd6b00bc3c6b0e8d2b5b
hallowslab <hallow.tech.lab@gmail.com>
2026-05-28 14:59:10 +0100

    utils

---
+++ pymap/utils.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+def build_logfile(job, task=None, user1=None, user2=None):
+    """
+    Builds the log filename based on the source host, destination host, and users.
+    Format: {source_host}_{dest_host}_{user1}--{user2}.log
+    """
+    u1 = user1 or (task.user1 if task else None)
+    u2 = user2 or (task.user2 if task else None)
+    return (
+        f"{job.source_host}_{job.dest_host}_"
+        f"{u1}--{u2}.log"
+    )
